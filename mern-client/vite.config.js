13850e4222d4d583d5d11f517ce15f3adc25d8f6
vengeance <143408321+HarshaL181818@users.noreply.github.com>
2025-03-31 15:31:15 +0530

    Update vite.config.js

--- mern-client/vite.config.js
+++ mern-client/vite.config.js
@@ -2,14 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-
+  plugins: [react()], // This should be at the top level
   server: {
-  host: '0.0.0.0',
-  port: 5173,
-  strictPort: true,
-  https: false,  // Ensure this is false
-    plugins: [react()],
-}
-
+    host: '0.0.0.0',
+    port: 5173,
+    strictPort: true,
+    https: false // Ensure this is false
+  }
 });
+
 
